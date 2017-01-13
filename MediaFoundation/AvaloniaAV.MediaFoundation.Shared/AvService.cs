93cd62b043d0445e15838377462c524fca65cd5d
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-01-13 14:48:39 -0600

    Updated UWP capture player to copy surfaces to a surface on the correct device. Changed device initialization to use the Avalonia DXGI device (created in AvaloniaUI/Avalonia#850).

--- MediaFoundation/AvaloniaAV.MediaFoundation.Shared/AvService.cs
+++ MediaFoundation/AvaloniaAV.MediaFoundation.Shared/AvService.cs
@@ -1,43 +1,31 @@
-﻿using SharpDX.Direct3D;
+﻿using Avalonia;
+using SharpDX.Direct3D;
 using SharpDX.Direct3D11;
 
 namespace AvaloniaAV.MediaFoundation
 {
     public class AvService
     {
-        private Device device;
+        public AvService()
+        {
+            using (var multithreaded = AvaloniaLocator.Current.GetService<SharpDX.DXGI.Device>().QueryInterface<DeviceMultithread>())
+            {
+                multithreaded.SetMultithreadProtected(true);
+            }
+        }
+
         public StreamPlayer GetStreamPlayer(int fps = 60)
         {
-            InitDevice();
-
-            return new StreamPlayer(device.QueryInterface<SharpDX.DXGI.Device>(), fps);
+            return new StreamPlayer(AvaloniaLocator.Current.GetService<SharpDX.DXGI.Device>(), fps);
         }
 
         public CapturePlayer GetCapturePlayer()
         {
 #if NETFX
-            InitDevice();
-            return new CapturePlayer(device.QueryInterface<SharpDX.DXGI.Device>());
+            return new CapturePlayer(AvaloniaLocator.Current.GetService<SharpDX.DXGI.Device>());
 #else
             return new CapturePlayer();
 #endif
-        }
-
-        private void InitDevice()
-        {
-            if (device != null) return;
-
-            device = new Device(
-                DriverType.Hardware,
-                DeviceCreationFlags.BgraSupport | DeviceCreationFlags.VideoSupport,
-                FeatureLevel.Level_11_1,
-                FeatureLevel.Level_12_0,
-                FeatureLevel.Level_12_1);
-
-            using (var multithread = device.QueryInterface<Multithread>())
-            {
-                multithread.SetMultithreadProtected(true);
-            }
         }
     }
 }
--- MediaFoundation/AvaloniaAV.MediaFoundation.UWP/CapturePlayer.cs
+++ MediaFoundation/AvaloniaAV.MediaFoundation.UWP/CapturePlayer.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Reactive.Subjects;
 using System.Runtime.InteropServices;
-using System.Text;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
-using System.Threading.Tasks;
+using Windows.Graphics.DirectX;
 using Windows.Graphics.DirectX.Direct3D11;
+using Windows.Graphics.Imaging;
 using Windows.Media.Capture;
+using Avalonia;
 using SharpDX;
+using SharpDX.Direct3D11;
 using SharpDX.DXGI;
 
 namespace AvaloniaAV.MediaFoundation
@@ -18,6 +20,30 @@ namespace AvaloniaAV.MediaFoundation
         [DllImport("D3D11.dll")]
         private static extern int GetDXGIInterfaceFromObject([MarshalAs(UnmanagedType.IInspectable)] object obj,
             Guid interfaceGuid, out IntPtr interfacePtr);
+
+        private readonly Subject<Surface> surfaceSubject = new Subject<Surface>();
+        public CapturePlayer()
+        {
+            CurrentSurface = surfaceSubject;
+        }
+
+        private static Surface CreatePresentedSurfaceForCurrentCapture(int width, int height)
+        {
+            var device = AvaloniaLocator.Current.GetService<SharpDX.DXGI.Device>();
+            using (var d3D11Device = device.QueryInterface<SharpDX.Direct3D11.Device>())
+            using (var texture = new Texture2D(d3D11Device,
+                new Texture2DDescription
+                {
+                    Width = width,
+                    Height = height,
+                    Format = Format.B8G8R8A8_UNorm,
+                    CpuAccessFlags = CpuAccessFlags.Read | CpuAccessFlags.Write,
+                }))
+
+            {
+                return texture.QueryInterface<Surface>();
+            }
+        }
 
         private static Surface GetDxgiSurface(IDirect3DSurface wrapperDSurface)
         {
@@ -29,30 +55,102 @@ namespace AvaloniaAV.MediaFoundation
 
         async partial void StartCaptureCore(SystemCamera camera, CancellationToken token)
         {
-            var surfaceSubject = new Subject<Surface>();
-            CurrentSurface = surfaceSubject;
-            
             using (var capture = new MediaCapture())
             {
                 await capture.InitializeAsync(new MediaCaptureInitializationSettings
                 {
                     VideoDeviceId = camera.SystemIdentifier,
                     SharingMode = MediaCaptureSharingMode.SharedReadOnly
-                });
+                }).AsTask(token).ConfigureAwait(false);
 
-                await capture.StartPreviewAsync();
+                await capture.StartPreviewAsync().AsTask(token).ConfigureAwait(false);
 
+                Surface surfaceToRender = null;
                 while (!token.IsCancellationRequested)
-                {
-                    var frame = await capture.GetPreviewFrameAsync();
+                {   
+                    var frame = await capture.GetPreviewFrameAsync().AsTask(token).ConfigureAwait(false);
+
+                    var bitmap = frame.SoftwareBitmap;
+                    if (bitmap != null)
+                    {
+                        RenderSoftwareBitmapToSurface(bitmap, ref surfaceToRender);
+                    }
 
                     var d3DSurface = frame.Direct3DSurface;
                     if (d3DSurface != null)
                     {
-                        surfaceSubject.OnNext(GetDxgiSurface(d3DSurface)); 
+                        RenderD3DSurface(d3DSurface, ref surfaceToRender);
+                    }
+                    if (surfaceToRender != null)
+                    {
+                        surfaceSubject.OnNext(surfaceToRender); 
                     }
                 }
-                await capture.StopPreviewAsync(); 
+                await capture.StopPreviewAsync().AsTask(token).ConfigureAwait(false); 
+            }
+        }
+
+        private static void RenderD3DSurface(IDirect3DSurface d3DSurface, ref Surface surfaceToRender)
+        {
+            using (d3DSurface)
+            {
+                // TODO: Support more BGRA formats
+                Debug.Assert(d3DSurface.Description.Format == DirectXPixelFormat.B8G8R8A8UIntNormalized);
+                if (surfaceToRender == null)
+                {
+                    surfaceToRender = CreatePresentedSurfaceForCurrentCapture(d3DSurface.Description.Width,
+                        d3DSurface.Description.Height);
+                }
+                using (var frameSurface = GetDxgiSurface(d3DSurface))
+                {
+                    // TODO: Copy within the GPU, not the CPU
+                    frameSurface.Map(SharpDX.DXGI.MapFlags.Read, out DataStream frameStream);
+                    surfaceToRender.Map(SharpDX.DXGI.MapFlags.Write, out DataStream surfaceStream);
+                    using (frameStream)
+                    using (surfaceStream)
+                    {
+                        frameStream.CopyTo(surfaceStream);
+                    }
+
+                    frameSurface.Unmap();
+                    surfaceToRender.Unmap();
+                }
+            }
+        }
+
+        private static void RenderSoftwareBitmapToSurface(SoftwareBitmap bitmap, ref Surface surfaceToRender)
+        {
+            using (bitmap)
+            {
+                var bgraBitmap = bitmap.BitmapPixelFormat == BitmapPixelFormat.Bgra8
+                    ? bitmap
+                    : SoftwareBitmap.Convert(bitmap, BitmapPixelFormat.Bgra8);
+                try
+                {
+                    if (surfaceToRender == null)
+                    {
+                        surfaceToRender = CreatePresentedSurfaceForCurrentCapture(bgraBitmap.PixelWidth,
+                            bgraBitmap.PixelHeight);
+                    }
+                    var rect = surfaceToRender.Map(SharpDX.DXGI.MapFlags.Write, out DataStream surfaceStream);
+                    var buffer = new Windows.Storage.Streams.Buffer((uint) (bgraBitmap.PixelHeight * rect.Pitch));
+                    bgraBitmap.CopyToBuffer(buffer);
+                    
+                    using (var bufferStream = buffer.AsStream())
+                    using(surfaceStream)
+                    {
+                        bufferStream.CopyTo(surfaceStream);
+                    }
+
+                    surfaceToRender.Unmap();
+                }
+                finally
+                {
+                    if (bgraBitmap != bitmap)
+                    {
+                        bgraBitmap.Dispose();
+                    }
+                }
             }
         }
     }
