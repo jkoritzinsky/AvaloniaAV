d4ee320579f89a60d1da33e68593afc0109e4bee
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-02-07 16:02:45 -0600

    Added specialized button for play/pause and updated the styles. Started work on the time tracking slider.

---
+++ AvaloniaAV/PlayPauseButton.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaAV
+{
+    public class PlayPauseButton : Button
+    {
+        public static readonly DirectProperty<PlayPauseButton, bool> IsPlayingProperty =
+            AvaloniaProperty.RegisterDirect<PlayPauseButton, bool>(nameof(IsPlaying),
+                button => button.IsPlaying,
+                (button, showPlay) => button.IsPlaying = showPlay);
+
+        public PlayPauseButton()
+        {
+            PseudoClass(IsPlayingProperty, ":playing");
+        }
+
+        private bool showPlay;
+        public bool IsPlaying
+        {
+            get
+            {
+                return showPlay;
+            }
+            set
+            {
+                SetAndRaise(IsPlayingProperty, ref showPlay, value);
+            }
+        }
+    }
+}
