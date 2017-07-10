a27351c92585b0e6b5e61da63e71fd74fb675a65
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-07-09 17:44:29 -0700

    Add initialization of the global DXGI device for render platforms that aren't Direct2D1 for MediaFoundation.

--- MediaFoundation/AvaloniaAV.MediaFoundation.Shared/MediaFoundationModule.cs
+++ MediaFoundation/AvaloniaAV.MediaFoundation.Shared/MediaFoundationModule.cs
@@ -23,11 +23,17 @@ namespace AvaloniaAV.MediaFoundation
 {
     using AvaloniaAV.Framebuffer;
     using AvaloniaAV.MediaFoundation.Framebuffer;
+    using SharpDX.DXGI;
 
     public class MediaFoundationModule
     {
         internal protected MediaFoundationModule()
         {
+            if (AvaloniaLocator.Current.GetService<Device>() == null)
+            {
+                AvaloniaLocator.CurrentMutable.Bind<Device>().ToConstant(CreateDxgiDevice());
+            }
+
             MediaManager.Startup();
             Application.Current.OnExit += (obj, args) => MediaManager.Shutdown();
 #if !REFERENCE
@@ -36,6 +42,31 @@ namespace AvaloniaAV.MediaFoundation
             AvaloniaLocator.CurrentMutable.Bind<AvService>().ToSingleton<AvService>();
 
             AvaloniaLocator.CurrentMutable.Bind<IPlatformFramebufferPlayerProvider>().ToSingleton<FramebufferCameraProvider>();
+        }
+
+        private Device CreateDxgiDevice()
+        {
+
+            var featureLevels = new[]
+            {
+                    SharpDX.Direct3D.FeatureLevel.Level_11_1,
+                    SharpDX.Direct3D.FeatureLevel.Level_11_0,
+                    SharpDX.Direct3D.FeatureLevel.Level_10_1,
+                    SharpDX.Direct3D.FeatureLevel.Level_10_0,
+                    SharpDX.Direct3D.FeatureLevel.Level_9_3,
+                    SharpDX.Direct3D.FeatureLevel.Level_9_2,
+                    SharpDX.Direct3D.FeatureLevel.Level_9_1,
+            };
+
+            using (var d3dDevice = new SharpDX.Direct3D11.Device(
+                SharpDX.Direct3D.DriverType.Hardware,
+                SharpDX.Direct3D11.DeviceCreationFlags.BgraSupport |
+                SharpDX.Direct3D11.DeviceCreationFlags.VideoSupport,
+                featureLevels))
+            {
+                return d3dDevice.QueryInterface<SharpDX.DXGI.Device>();
+            }
+
         }
     }
 }
